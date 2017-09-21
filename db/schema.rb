72ff7bc0a2d2342a760ffbd6708f591a7eed22df
Hannie Dong <hdong92@gmail.com>
2017-09-21 15:42:45 -0700

    Add image feature.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170920224355) do
+ActiveRecord::Schema.define(version: 20170921214751) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,12 @@ ActiveRecord::Schema.define(version: 20170920224355) do
     t.string "title"
     t.string "category"
     t.text "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "images", force: :cascade do |t|
+    t.string "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
